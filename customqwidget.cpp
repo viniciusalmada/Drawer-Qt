d78a089f8d4006a9fca0c8081a96e2e4037a94b6
Debian-Ryzen <viniciusalmada10@gmail.com>
2020-11-23 16:22:29 -0300

    added drawer functions to paintEvent

--- customqwidget.cpp
+++ customqwidget.cpp
@@ -9,8 +9,15 @@ void CustomQWidget::paintEvent(QPaintEvent* event) {
 	qDebug("called");
 	QPainter painter(this);
 	painter.setPen(Qt::blue);
+	int side = qMin(width(), height());
+	int x = width() - side / 2;
+	int y = height() - side / 2;
+	painter.setViewport(x, y, side, side);
+	painter.drawLine(0, 0, 50, 0);
+	painter.setWindow(QRect(-50, -50, 100, 100));
+	
 	painter.setFont(QFont("Roboto", 30));
-	painter.drawText(rect(), Qt::AlignCenter, "Qt");
+	painter.drawText(0, 0, "Qt");
 }
 
 void CustomQWidget::mouseMoveEvent(QMouseEvent* event) {
